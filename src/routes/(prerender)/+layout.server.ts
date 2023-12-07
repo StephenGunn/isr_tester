9a4848fc84baafd306b0e593818d66358fd98365
Stephen <stephengunn@gmail.com>
2023-12-06 21:12:54 -0600

    one folder trick

--- src/routes/(prerender)/+layout.server.ts
+++
@@ -1,7 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-
-export const load = (async () => {
-    return {
-        layoutnumber: Math.floor(Math.random() * 20)
-    }
-}) satisfies LayoutServerLoad;
