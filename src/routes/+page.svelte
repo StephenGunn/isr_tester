6debd3c4011cd1c6cd077823080a5980f68c121d
Stephen <stephengunn@gmail.com>
2023-12-06 20:49:43 -0600

    isr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	export let data;
+</script>
+
+<h1>Homepage</h1>
+<h3>page data: {data.number ?? 999999999}</h3>
+<h3>layout data: {data.layoutnumber ?? 88888888888}</h3>
+
+<a href="/isr">ISR</a>
--- src/routes/isr/+page.svelte
+++ src/routes/isr/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import { invalidateAll } from '$app/navigation';
 	export let data;
+	const inval = async () => await invalidateAll();
 </script>
 
-<h1>ISR tester</h1>
+<h1>/ISR tester</h1>
 <h3>page data: {data.number ?? 999999999}</h3>
 <h3>layout data: {data.layoutnumber ?? 88888888888}</h3>
+<a href="/">home</a>
+<button on:click={inval}>Invalidate</button>
